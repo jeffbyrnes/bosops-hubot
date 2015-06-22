781f454decab3d3ec045fba9a966566503a23b9a
Matt Jones <mattjones@yieldbot.com>
2015-06-22 16:56:26 -0400

    add task to get latest meetup from group

--- scripts/meetups.js
+++ scripts/meetups.js
@@ -19,32 +19,44 @@ module.exports = function(robot) {
     }
   });
 
+  // This will take a known meeting from the list and search for the latest
+  // event and give revelent details
   robot.respond(/When is the next (.*) meetup\?/i, function(res) {
-    var meetup, api_call, api_key;
+    var fs = require('fs');
+    var meetup, api_call, api_key, meetup_id;
     api_key = process.env['MEETUP_TOKEN'];
     api_key = "241a7624282f51133b6ca7236c4d43"
     meetup = res.match[1];
 
-    var known_meetups = new Array();
-    known_meetups["name"] = "Boston Devops";
-    known_meetups["id"] = "223338062";
+    file_text = fs.readFileSync('lib/group_list.json').toString()
+    groups = JSON.parse(file_text)
+    var arrayLength = groups.length;
+    for (var i = 0; i < arrayLength; i++) {
+      if (groups[i]['name'] == meetup) {
+        res.send("Looking up details for the latest " + meetup + " meetup")
+        meetup_id = groups[i]['id']
+      }
+    }
+    if (meetup_id == '') {
+      res.send("I don't know anything about that group")
+    }
 
     this.exec = require('child_process').exec;
 
-    api_call = "curl " + "\"https://api.meetup.com/2/223338062?key=" + api_key + "&sign=true&photo-host=public&page=20\"";
-    res.send("Looking for the next " + meetup + "...");
+    api_call = "curl " + "\"https://api.meetup.com/2/events?key=" + api_key + "&sign=true&photo-host=public&group_id=" + meetup_id + "&page=20\"";
     this.exec(api_call, function(error, stdout, stderror) {
-      var out;
-      out = JSON.parse(stdout);
-      var con_date = new Date(out['time']);
-      res.send("Meetup: " + meetup);
-      res.send("Location: " + out['venue']['name']);
-      res.send("Address: " + out['venue']['address_1']);
-      res.send("City: " + out['venue']['city']);
-      res.send("Date: " + con_date);
-      res.send("Url: " + out['event_url']);
+      var out = JSON.parse(stdout);
+      if (typeof out['results'][0] != 'undefined') {
+        var con_date = new Date(out['results'][0]['time']);
+        res.send("Meetup: " + meetup);
+        res.send("Location: " + out['results'][0]['venue']['name']);
+        res.send("Address: " + out['results'][0]['venue']['address_1']);
+        res.send("City: " + out['results'][0]['venue']['city']);
+        res.send("Date: " + con_date);
+        res.send("Url: " + out['results'][0]['event_url']);
+      } else {
+        res.send("There are no events currently scheduled for this group")
+      }
     });
   });
 };
-
-//https://api.meetup.com/2/groups?&sign=true&photo-host=public&group_urlname=Boston-Devops&page=20
