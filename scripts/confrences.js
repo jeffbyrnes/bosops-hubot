70a3208e42c7183bcf7f751e522ac07ac6c2b6eb
Matt Jones <mattjones@yieldbot.com>
2015-06-24 01:16:58 -0400

    add confrence task to get data

--- scripts/confrences.js
+++ scripts/confrences.js
@@ -16,4 +16,46 @@ module.exports = function(robot) {
       res.send(confs[i]['name'] + "   " + confs[i]['url'])
     }
   });
+
+  robot.respond(/What are the details of the (.*) conference\?/i, function(res) {
+    var fs = require('fs');
+    var conf;
+    conf = res.match[1];
+
+    function IsJsonString(str) {
+      try {
+        out = JSON.parse(str);
+      } catch (e) {
+        res.send(" There is no information available for this conference");
+        process.exit();
+      }
+      return out;
+    }
+
+    file_text = fs.readFileSync('lib/conf_list.json').toString();
+    this.exec = require('child_process').exec;
+    confs = JSON.parse(file_text);
+    var arrayLength = confs.length;
+    for (var i = 0; i < arrayLength; i++) {
+      if (confs[i]['name'] == conf) {
+        res.send("Looking up details for the " + conf + " conference")
+        var conf_url = confs[i]['url']
+        var cmd = "curl " +  conf_url + "/hubot_spec.json";
+        this.exec(cmd, function(error, stdout, stderror) {
+          res.send("test")
+          out = IsJsonString(stdout)
+          res.send(out['conference']['name']);
+          res.send("Conference dates: " + out['conference']['dates']['conf start'] + " - " +  out['conference']['dates']['conf end']);
+          res.send("Call for papers: " + out['conference']['dates']['cfp start'] + " - " +  out['conference']['dates']['cfp end']);
+        });
+      };
+    };
+    if (typeof conf_url === 'undefined') {
+      res.send("I don't know anything about that conference.")
+      res.send("I know of the following conferences:")
+      for (var i = 0; i < arrayLength; i++) {
+        res.send(confs[i]['name'])
+      };
+    }
+  });
 };
